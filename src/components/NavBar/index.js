314f344579e5fa436e57866cbaf3911920dc9a53
apubhadaram <abepadua22@gmail.com>
2021-12-10 03:51:31 +0800

    Add a background on scroll breakpoint

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,3 +1,5 @@
+// FUNCTIONS
+import { useEffect } from 'react';
 // COMPONENTS
 import { Navbar } from "react-bootstrap"
 import { SearchBar, NavItems } from "./NavElems"
@@ -6,7 +8,24 @@ import Logo from "../../svgs/logo_white.svg"
 import { useLocation } from "react-router-dom"
 
 const NavBar = () => {
-  const location = useLocation()
+  const location = useLocation();
+
+  useEffect(() => {
+    // add a showbg classname to navbar if scrolled past 100px
+    const nav = document.querySelector("#main-nav")
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight / 5) {
+        nav.classList.remove("home")
+      } else {
+        nav.classList.add("home")
+      }
+    }
+    if (location.pathname === "/")
+      window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, []);
 
   return (
     <Navbar id="main-nav" className={location.pathname === "/" ? "home" : ""}>
